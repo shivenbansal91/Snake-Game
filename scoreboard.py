a67915dc9863c695688f01cf47f4e878aa98c1b4
Your Name <your-email@example.com>
2025-07-03 17:16:46 +0530

    add high score

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -14,14 +15,20 @@ class Score(Turtle):
 
 
     def update_score(self):
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font= FONT)
+        self.clear()
+        self.write(f"Score = {self.score} HIGH SCORE = {self.high_score}", align=ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font= FONT)
+    def reset(self):
+        if(self.score> self.high_score):
+            self.high_score = Score
+        self.score = 0
+        self.update_score()
+    #def game_over(self):
+     #   self.goto(0,0)
+     #   self.write("GAME OVER", align=ALIGNMENT, font= FONT)
 
     def increase_score(self):
         
         self.score += 1
-        self.clear()
+        
         self.update_score()
